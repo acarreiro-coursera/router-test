7de41b398facd27ad3855030e28b61a1465e7a21
Andrew Carreiro <acarreiro@coursera.org>
2023-04-21 10:46:21 -0400

    test for heartbeat feasibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  Outlet,
+  useMatches,
+} from "react-router-dom";
+
+const Root = () => (
+  <div>
+    <div>root!</div>
+    <Outlet />
+  </div>
+);
+
+const CoolComponent = ({ message }) => {
+  const match = useMatches();
+  console.log(match);
+
+  return (<div>{message}</div>)
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+
+      <Route id='the-cool-root' path="/" Component={Root}>
+        <Route id='id-hello' path="hello" element={<CoolComponent message="hello" />} />
+        <Route id='id-goodbye' path="goodbye" element={<CoolComponent message="goodbye" />} />
+      </Route>
+
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
